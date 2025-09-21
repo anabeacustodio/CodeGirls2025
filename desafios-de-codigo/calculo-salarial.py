b22ce34a9b5ddd2616cab57df4f696a94497aa81
Ana Beatriz Custódio <anabeatriz.custodio.oliveira@gmail.com>
2025-09-21 15:23:35 -0300

    ajuste sintaxe

--- desafios-de-codigo/calculo-salarial.py
+++ desafios-de-codigo/calculo-salarial.py
@@ -7,9 +7,9 @@ def calcular_imposto(salario):
     aliquota = 0.00
     if (salario >= 0 and salario <=1100):
         aliquota = 0.05
-        elif (salario >= 1100.01 and salario <= 2500):
+    elif (salario >= 1100.01 and salario <= 2500):
             aliquota = 0.10
-        else:
+    else:
             aliquota = 0.15
     return aliquota * salario
 
